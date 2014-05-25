827f35efd166924c39f176af1ccc970b57a03090
U-sabonisI5\sabonis <sabonis@sabonisI5.(none)>
2014-05-25 15:54:11 +0800

    Downloader implemented.

--- src/com/demo/VideoDownloader.java
+++ src/com/demo/VideoDownloader.java
@@ -1,5 +1,12 @@
 package com.demo;
 
+import java.io.BufferedInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class VideoDownloader {
 
     /**
@@ -10,7 +17,20 @@ public class VideoDownloader {
      * @param url
      */
     public void saveVideo(String filename, String url) {
-
+        FileOutputStream fout = null;
+        try {
+            BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+            final byte data[] = new byte[1024];
+            int count;
+            while ((count = in.read(data, 0, 1024)) != -1) {
+                fout.write(data, 0, count);
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-
 }
